d00593f9c1bf3919bd6438dd09e59be673ff9789
Chuyuan(Cyril) Liu <linkliucy@foxmail.com>
2022-08-25 23:27:45 +0800

    feat: enbale pass param in request

--- src/main/java/com/example/xidpspringrestfuldemo/GreetingController.java
+++ src/main/java/com/example/xidpspringrestfuldemo/GreetingController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class GreetingController {
+
+    private  static final String helloTemplate = "Hello, %s!";
+    private final AtomicLong automaticId = new AtomicLong();
     @GetMapping("/greeting")
-    public Greeting greeting() {
-        return new Greeting(1L, "Hello World!");
+    public Greeting greeting(@RequestParam(value = "helloname",defaultValue = "World") String name) {
+        return new Greeting(automaticId.incrementAndGet(), String.format(helloTemplate,name));
     }
 }
